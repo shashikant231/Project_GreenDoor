e23c0609f0082d95881a6dd81d8682525c9acb6c
shashikant231 <shashikantching@gmail.com>
2021-08-18 17:51:31 +0530

    changed response of user profile

--- description_api/serializers.py
+++ description_api/serializers.py
@@ -1,5 +1,6 @@
 from django.db.models import fields
 from .models import DescriptionModel,UserProfileModel
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from .models import UserProfileModel
@@ -19,4 +20,11 @@ class DescriptionModelSerializer(serializers.ModelSerializer):
 class UserProfileModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfileModel
-        fields = '__all__'         
+        fields = '__all__'   
+
+    def to_representation(self, instance):
+        res = super().to_representation(instance)
+        email = User.objects.filter(username = instance.user.username).values('email')
+        res['email'] = email[0]['email']
+        res['username'] = instance.user.username
+        return res      
