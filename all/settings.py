e336dc80f199aa08b63ead6c5da4b4ea4749bd10
shashikant231 <shashikantching@gmail.com>
2021-08-20 16:28:55 +0530

    removed email credentials

--- all/settings.py
+++ all/settings.py
@@ -166,5 +166,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'trackeriss511@gmail.com'
-EMAIL_HOST_PASSWORD = "Greendoor@cr7"
+EMAIL_HOST_USER = 'emial'
+EMAIL_HOST_PASSWORD = "email"
