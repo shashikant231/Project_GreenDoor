cee07a8688aaf320f96cae5c8c17264cc2e22085
shashikant231 <shashikantching@gmail.com>
2021-09-25 15:15:19 +0530

    removed status from input fields

--- blog/serializers.py
+++ blog/serializers.py
@@ -6,4 +6,4 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ("title","author","content","status")
+        fields = ("title","author","content")
